20d254c507ec7eea1d67c80b13f740e8e0e8abae
LouesCS <luizlazari135@gmail.com>
2025-08-01 21:07:55 -0300

    Update

--- cypress/e2e/desbravador-reserva/reserva.cy.js
+++ cypress/e2e/desbravador-reserva/reserva.cy.js
@@ -1,49 +1,56 @@
 Cypress.on('uncaught:exception', (err, runnable) => {
-  return false; // Ignora erros não tratados da aplicação
+  // Ignora erros não tratados da aplicação
+  return false;
 });
 
-describe('Reserva de hotel', () => {
-  it('deve realizar uma reserva para 3 dias e efetuar o pagamento', () => {
-    const email = `teste+${Date.now()}@gmail.com`;
-    const senha = 'senha123';
+it('deve realizar uma reserva para 3 dias e efetuar o pagamento', () => {
+  // Acessa o site de reservas
+  cy.visit('https://reservas.desbravador.com.br/1111');
 
-    cy.visit('https://reservas.desbravador.com.br/1111');
+  // Aguarda carregamento e seleciona datas (10 a 12)
+  cy.wait(3000);
+  cy.get('.datepickerDays td:not(.datepickerNotInMonth) a').contains('10').click();
+  cy.get('.datepickerDays td:not(.datepickerNotInMonth) a').contains('12').click();
 
-    // Seleciona datas
-    cy.get('.datepickerDays', { timeout: 10000 }).should('be.visible');
-    cy.selecionarDatas('10', '12');
+  // Seleciona 2 adultos e 1 criança grátis
+  cy.get('#cdadultos').select('2');
+  cy.get('#cdchdfree').select('1');
 
-    // Seleciona hóspedes
-    cy.get('#cdadultos').select('2');
-    cy.get('#cdchdfree').select('1');
+  // Avança para a seleção de tarifas
+  cy.get('#button').click();
+  cy.get('#btn_tarifas-ST1-TESTEAR').click();
+  cy.get('#bt_compra-ST1').click();
 
-    // Seleciona tarifa
-    cy.get('#button').click();
-    cy.get('#btn_tarifas-ST1-TESTEAR').click();
-    cy.get('#bt_compra-ST1').click();
+  // Clica no botão para pagar
+  cy.get('#bt_pagar > .btn').click();
 
-    // Pagar
-    cy.get('#bt_pagar > .btn').click();
+  // Abre o formulário de cadastro
+  cy.get('#opcoes_autenticacao > .float-right').click();
 
-    // Cadastro
-    cy.get('#opcoes_autenticacao > .float-right').click();
-    cy.preencherCadastro(email, senha);
+  // Preenche o formulário de cadastro normal
+  cy.get('#nmpessoafn').type('Luiz');
+  cy.get('#nmpessoasn').type('Lazari');
+  cy.get('#nmlogin').type('luiz@gmail.com');
+  cy.get('#nmlogin1').type('luiz@gmail.com');
+  cy.get('#nmsenha').type('ha123477g');
+  cy.get('#nmsenha2').type('ha123477g');
+  cy.get('#button').click();
 
-    // Login (caso já exista)
-    cy.fazerLogin(email, senha);
-    cy.get('.largura_site > .cx').click();
+  // Caso já possua cadastro, realiza o login
+  cy.get('.float-left > a > strong').click();
+  cy.get('#usuario').type('luiz@gmail.com');
+  cy.get('#senha').type('ha123477g');
+  cy.get('.largura_site > .cx').click();
 
-    // Aceitar termos no iframe
-      cy.get('#frame_politicas')
-      .its('0.contentDocument.body').should('not.be.empty')
-      .then(cy.wrap)
-      .within(() => {
-        // Marca o checkbox 
+  // Aqui tem o iframe de politica, onde ele aceita os termos e continua a reserva
+  cy.get('#frame_politicas')
+    .its('0.contentDocument.body').should('not.be.empty')
+    .then(cy.wrap)
+    .within(() => {
+      // Marca o checkbox 
       cy.get('label.checkbox').click({ force: true });
-    });
 
-    // Verifica se finalizou a reserva com sucesso
-    cy.contains('Reserva confirmada', { timeout: 10000 }).should('be.visible');
-    cy.url().should('include', '/reserva-confirmada');
-  });
+      // Clica no botão para continuar a reserva
+      cy.contains('button', 'Continuar Reserva').click({ force: true });
+    });
 });
